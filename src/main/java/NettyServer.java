96b0bb5a9396e69bc0801cb2a59f4f3ad7378ad3
OKBABA <minchen.wei@qq.com>
2018-05-08 22:26:47 +0800

    test

---
+++ src/main/java/NettyServer.java
@@ -0,0 +1,48 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+    private int port;
+
+    public NettyServer(int port) {
+        this.port = port;
+    }
+
+    public void run() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); //用于处理服务器端接收客户端连接
+        EventLoopGroup workerGroup = new NioEventLoopGroup(); //进行网络通信（读写）
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap(); //辅助工具类，用于服务器通道的一系列配置
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(
+                    new ChannelInitializer<SocketChannel>() { //配置具体的数据处理方式
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new ServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128) //设置TCP缓冲区
+                    .option(ChannelOption.SO_SNDBUF, 32 * 1024) //设置发送数据缓冲大小
+                    .option(ChannelOption.SO_RCVBUF, 32 * 1024) //设置接受数据缓冲大小
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); //保持连接
+            ChannelFuture future = bootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new NettyServer(8379).run();
+    }
+}
